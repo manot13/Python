d7bb70017519eff53b1a1304dda0c2c1a6b10048
Nathan <63300192+manot13@users.noreply.github.com>
2021-04-06 17:06:55 -0300

    Programa que converte temperaturas
    Fahrenheit para Celcius

---
+++ temperatura.py
@@ -0,0 +1,6 @@
+f = float(input("Digite a temperatura em graus Fahrenheit: "))
+
+C = 5*(f-32)/9
+
+print("Valor em graus Fahrenheit: {:.2f} °F" .format(f))
+print("Valor em graus Celsius: {:.4f} °C" .format(C))
